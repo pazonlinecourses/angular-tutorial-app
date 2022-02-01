5ce6d4b545bf3ab07fde4642116d991f6baec54a
PazJal <30651575+PazJal@users.noreply.github.com>
2022-02-01 18:07:34 +0200

    feat: generate general app structure
    add 3 main sections:
    recipe-book
    shoppping-list
    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingViewComponent } from './feature/shopping-list/shopping-view/shopping-view.component';
+import { ShoppingEditComponent } from './feature/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './feature/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './feature/recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './feature/recipe-book/recipe-detail/recipe-detail.component';
+import { AppHeaderComponent } from './feature/app-header/app-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingViewComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/feature/shopping-list/shopping-view/shopping-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-view',
+  templateUrl: './shopping-view.component.html',
+  styleUrls: ['./shopping-view.component.css']
+})
+export class ShoppingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
